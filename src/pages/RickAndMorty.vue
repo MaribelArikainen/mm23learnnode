bff5166f0283f8ef8672ee2f72ab083ce7e398d5
Maribel Arikainen <maribel.arikainen@tptlive.ee>
2025-09-07 13:06:13 +0300

    charachters

---
+++ src/pages/RickAndMorty.vue
@@ -0,0 +1,24 @@
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+import CharacterCard from '../components/CharacterCard.vue';
+let characters = ref([]);
+
+getCharacters('https://rickandmortyapi.com/api/character');
+function getCharacters(url){
+    axios.get(url).then(response => {
+        console.log(response);
+        characters.value = response.data.results;
+    });
+}
+</script>
+<template>
+<div class="container">
+    <div class="columns is-multiline">
+        <div class="column is-one-quarter" v-for="character in characters">
+            <CharacterCard :character="character"></CharacterCard>
+        </div>
+    </div>
+    
+</div>
+</template>
