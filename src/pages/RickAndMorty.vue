6bbb2d4ff0002835aa4c86a64c8b70af73372bed
Maribel Arikainen <maribel.arikainen@tptlive.ee>
2025-09-07 13:35:02 +0300

    searchbar

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -1,24 +1,61 @@
 <script setup>
-import axios from 'axios';
-import { ref } from 'vue';
-import CharacterCard from '../components/CharacterCard.vue';
+import axios from "axios";
+import { ref } from "vue";
+import CharacterCard from "../components/CharacterCard.vue";
 let characters = ref([]);
+let info = ref({});
+let searchValue = ref('');
+getCharacters("https://rickandmortyapi.com/api/character");
 
-getCharacters('https://rickandmortyapi.com/api/character');
-function getCharacters(url){
-    axios.get(url).then(response => {
-        console.log(response);
-        characters.value = response.data.results;
+function getCharacters(url) {
+  axios
+    .get(url, {
+      params: {
+        name: searchValue.value,
+      },
+    })
+    .then((response) => {
+      console.log(response);
+      characters.value = response.data.results;
+      info.value = response.data.info;
     });
+}
+
+function next() {
+  getCharacters(info.value.next);
+}
+function prev() {
+  getCharacters(info.value.prev);
+}
+function search(){
+    getCharacters("https://rickandmortyapi.com/api/character");
 }
 </script>
 <template>
-<div class="container">
-    <div class="columns is-multiline">
-        <div class="column is-one-quarter" v-for="character in characters">
-            <CharacterCard :character="character"></CharacterCard>
+  <div class="container">
+    <div class="field has-addons">
+        <div class="control is-expanded">
+            <input v-model="searchValue" class="input" type="text" placeholder="Search name">
+        </div>
+        <div class="control">
+            <button class="button is-info" @click="search">
+                Search
+            </button>
+        </div>
+    </div>
+    <div class="field has-addons">
+        <div class="control">
+            <button class="button is-primary" @click="prev" :disabled="!info.prev">Prev</button>
+        </div>
+        <div class="control">
+            <button class="button is-primary" @click="next" :disabled="!info.next">Next</button>
         </div>
     </div>
     
-</div>
+    <div class="columns is-multiline">
+      <div class="column is-one-quarter" v-for="character in characters">
+        <CharacterCard :character="character"></CharacterCard>
+      </div>
+    </div>
+  </div>
 </template>
