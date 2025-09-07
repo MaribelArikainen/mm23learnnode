da25eefa8e66c279f8c5dd8dee6517e7a1b6d874
Maribel Arikainen <maribel.arikainen@tptlive.ee>
2025-09-07 10:35:25 +0300

    Notification eraldi faili

---
+++ src/pages/NotificationExample.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Notification color="danger">
+      Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum
+      dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut,
+      porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam
+      gravida purus diam, et dictum <a>felis venenatis</a> efficitur.
+    </Notification>
+  </div>
+</template>
+
+<script>
+import Notification from "./components/Notification.vue";
+
+export default {
+  components: {
+    Notification,
+  },
+};
+</script>
